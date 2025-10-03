32aeb3c21035406fb5b97ea2d6f393f4d1cdc146
Ivan <ivangyr321@gmail.com>
2025-10-03 03:27:34 -0500

    feat: agregar componente de logo en el encabezado

---
+++ src/lib/components/Header/Logo.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import logo from '$lib/assets/logo.svg';
+</script>
+
+<a href="/" class="flex cursor-pointer justify-center">
+	<img src={logo} alt="Lenoir Logo" class="h-12 object-contain lg:h-16" />
+</a>
