798a62b0e67d3c15465806c78256edfee995039b
Ivan <ivangyr321@gmail.com>
2025-10-03 04:43:34 -0500

    fix: corregir la importación de componentes Header y Footer para que respeten la convención de nombres

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import SEO from '$lib/components/SEO.svelte';
-	import Header from '$lib/components/Header/Header.svelte';
-	import Footer from '$lib/components/Footer/Footer.svelte';
+	import Header from '$lib/components/header/Header.svelte';
+	import Footer from '$lib/components/footer/Footer.svelte';
 
 	let { children } = $props();
 </script>
