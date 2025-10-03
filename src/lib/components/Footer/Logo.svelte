52253f71c51c783e25b4669fe0429ea6a712154a
Ivan <ivangyr321@gmail.com>
2025-10-03 03:27:06 -0500

    feat: agregar componente de logo al pie de página

---
+++ src/lib/components/Footer/Logo.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import logoWhite from '$lib/assets/logo-white.svg';
+</script>
+
+<a href="/" class=" flex cursor-pointer">
+	<img src={logoWhite} alt="Lenoir Logo" class="h-12 object-contain lg:h-16" />
+</a>
